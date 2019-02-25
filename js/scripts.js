be65056b7da4b473bde85017d693aec945b9aaf0
Leah Gakii <leahgakii@gmail.com>
2019-02-25 16:41:44 +0300

    Add changes on scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,26 @@
-//Business logic
-function sum(q1, q2, q3, q4) {
-  return q1 + q2 + q3 + q4;
+function addition(q1,q2,q3){
+   return q1+q2+q3;
 }
 
-// user logic
 $(document).ready(function(){
-  $('#quiz').submit(function(event){
-    event.preventDefault();
-    $("#quiz").hide();
+   $("#quiz").submit(function(event){
+       event.preventDefault();
+       var q1=parseInt($("input:radio[name=choice1]:checked").val());
+       var q2 = parseInt($("input:radio[name=choice2]:checked").val());
+       var q3 = parseInt($("input:radio[name=choice3]:checked").val());
 
-      var q1 = parseInt($("input:radio[name=choice1]:checked").val());
-      var q2 = parseInt($("input:radio[name=choice2]:checked").val());
-      var q3 = parseInt($("input:radio[name=choice3]:checked").val());
-      var q4 = parseInt($("input:radio[name=choice4]:checked").val());
-      var total =sum(q1, q2, q3, q4);
+       var results = addition(q1,q2,q3);
 
+       $("#output").text(results + "%");
 
+var percentage = (results/100)*100;
 
-
-      var percentage = (total/50)*100;
-      $('#output2').text(percentage);
-      if(percentage>80 && percentage<=100){
-        $("#output2").text(percentage + "%" + "Excellent");
-      } else if (percentage>=50 && percentage<=80){
-        $("#output2").text(percentage + "%" + "Fair Results");
-      } else{
-        $("#output2").text(percentage + "%" + "failed! Retake test");
-      }
-    });
-  });
+       if(percentage>80 && percentage<=100){
+           $("#output2").text("Excellent");
+       } else if (percentage>=50 && percentage<=80){
+           $("#output2").text("Fair Results");
+       } else{
+           $("#output2").text("Retake test");
+       }
+   });
+})
