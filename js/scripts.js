b880dac6a699896a1ccb5d0b075b6d1d4ede7f8d
Leah Gakii <leahgakii@gmail.com>
2019-02-25 10:24:29 +0300

    Submit box

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var answer = function(answer1, answer2, answer3, answer4) {
+  return answer1 + answer2 + answer3 + answer4;
+};
+var answer1 = parseInt(prompt("wrong:"));
+var number2 = parseInt(prompt("wrong:"));
+var answer3 = parseInt(prompt("wrong:"));
+var answer4 = parseInt(prompt("wrong:"));
+var result = add(number1, number2, number4);
+alert(toobad);
